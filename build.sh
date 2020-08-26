b4897362365c1c396766308fbf8ac98fa3c4f6f2
Richard Malbec <richard@voxelmed.ai>
2020-08-26 14:41:47 -0400

    ci: test emconfigure

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 mkdir -p build
 #(cd build && emconfigure cmake -DCMAKE_BUILD_TYPE=Debug ..) &&
 ls
-# (cd build && emconfigure cmake ..) &&
-(cd build && emcmake cmake -G Unix Makefiles ..) &&
+(cd build && emconfigure cmake ..) &&
+# (cd build && emcmake cmake -G Unix Makefiles ..) &&
 (cd build && ls) &&
 (cd build && emmake make VERBOSE=1 -j 16) &&
 cp ./build/extern/openjpeg/bin/openjpegjs.js ./dist && 
