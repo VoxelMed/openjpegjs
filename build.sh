b722dc89266356fd71310ee09e2c1ddc09d1e0eb
Richard Malbec <richard@voxelmed.ai>
2020-08-26 13:30:40 -0400

    ci: revert to previous command

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 #rm -rf build
 mkdir -p build
 #(cd build && emconfigure cmake -DCMAKE_BUILD_TYPE=Debug ..) &&
-# Old: (cd build && emconfigure cmake ..) &&
-(cd build && emcmake make .) &&
+(cd build && emconfigure cmake ..) &&
+# (cd build && emcmake make .) &&
 (cd build && emmake make VERBOSE=1 -j 16) &&
 cp ./build/extern/openjpeg/bin/openjpegjs.js ./dist && 
 cp ./build/extern/openjpeg/bin/openjpegjs.js.mem ./dist &&
