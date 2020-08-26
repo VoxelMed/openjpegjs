02ba9d7fc7e23ad270dc9613827c2fbd67cc0168
Richard Malbec <richard@voxelmed.ai>
2020-08-26 13:51:12 -0400

    ci: try listing files

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-#rm -rf build
 mkdir -p build
 #(cd build && emconfigure cmake -DCMAKE_BUILD_TYPE=Debug ..) &&
-(cd build && emconfigure cmake ..) &&
-# (cd build && emcmake make .) &&
+ls
+# (cd build && emconfigure cmake ..) &&
+(cd build && emcmake make ..) &&
+ls
 (cd build && emmake make VERBOSE=1 -j 16) &&
 cp ./build/extern/openjpeg/bin/openjpegjs.js ./dist && 
 cp ./build/extern/openjpeg/bin/openjpegjs.js.mem ./dist &&
