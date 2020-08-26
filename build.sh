e9fe556865742af4cfce8b362fd0964669c03ce5
Richard Malbec <richard@voxelmed.ai>
2020-08-26 12:52:08 -0400

    ci: updates to reflect devcontainer differences

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 mkdir -p build
 #(cd build && emconfigure cmake -DCMAKE_BUILD_TYPE=Debug ..) &&
 # Old: (cd build && emconfigure cmake ..) &&
-(cd build && emcmake make ..) &&
+(cd build && emcmake make .) &&
 (cd build && emmake make VERBOSE=1 -j 16) &&
 cp ./build/extern/openjpeg/bin/openjpegjs.js ./dist && 
 cp ./build/extern/openjpeg/bin/openjpegjs.js.mem ./dist &&
