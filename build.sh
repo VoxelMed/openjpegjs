68bd9264be111a08acee639e9cd3718057eb4090
Richard Malbec <richard@voxelmed.ai>
2020-08-27 10:33:06 -0400

    ci: use emcmake, a wrapper for emconfigure

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 mkdir -p build # comment out for local build container
 #(cd build && emcmake cmake -DCMAKE_BUILD_TYPE=Debug ..) &&
-# (cd build && emcmake cmake ..) &&
-(cd build && emconfigure cmake ..) &&
+
+# This worked in: trzeci/emscripten:1.39.4-fastcomp
+# but not w/ newer versions of emscripten
+# (cd build && emconfigure cmake ..) &&
+
+(cd build && emcmake cmake ..) &&
 (cd build && emmake make VERBOSE=1 -j 16) &&
 cp ./build/extern/openjpeg/bin/openjpegjs.js ./dist && 
 cp ./build/extern/openjpeg/bin/openjpegjs.js.mem ./dist &&
