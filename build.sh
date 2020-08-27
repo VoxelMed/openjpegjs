f01999d5b1b2dac06265cec48ab5d2f4a3b32d1f
Richard Malbec <richard@voxelmed.ai>
2020-08-27 10:17:50 -0400

    ci: try older -fastcomp image that was likely the :latest default at time of devcontainer creation

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 mkdir -p build # comment out for local build container
 #(cd build && emcmake cmake -DCMAKE_BUILD_TYPE=Debug ..) &&
-(cd build && emcmake cmake --std=c++17 ..) &&
+# (cd build && emcmake cmake ..) &&
+(cd build && emconfigure cmake ..) &&
 (cd build && emmake make VERBOSE=1 -j 16) &&
 cp ./build/extern/openjpeg/bin/openjpegjs.js ./dist && 
 cp ./build/extern/openjpeg/bin/openjpegjs.js.mem ./dist &&
