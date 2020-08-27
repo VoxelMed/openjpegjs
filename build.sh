41be125f575bf0287d6a638dd3d87b6e2e2fd014
Richard Malbec <richard@voxelmed.ai>
2020-08-27 16:25:25 -0400

    ci: very hacky -- just run it twice?

--- build.sh
+++ build.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
-mkdir -p build # comment out for local build container
+# Disable exit on non 0
+set +e
+mkdir -p build
 #(cd build && emcmake cmake -DCMAKE_BUILD_TYPE=Debug ..) &&
 # This worked in: trzeci/emscripten:1.39.4-fastcomp
 # but not w/ newer versions of emscripten
 # (cd build && emconfigure cmake ..) &&
+# ~ Build Native
+# (cd build && cmake -DCMAKE_BUILD_TYPE=Debug ..) &&
+# (cd build && make VERBOSE=1 -j 8) &&
+# (build/extern/openjpeg/bin/cpptest)
+# ~ Build WASM/JS
+(cd build && emcmake cmake ..) || echo 0 &&
 (cd build && emcmake cmake ..) &&
 (cd build && emmake make VERBOSE=1 -j 16) &&
 cp ./build/extern/openjpeg/bin/openjpegjs.js ./dist && 
